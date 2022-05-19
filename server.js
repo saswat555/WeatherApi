5f44b60a4a7087a3ee438c5933a5db8bedf7a1e5
saswat555 <sapa19is@cmrit.ac.in>
2022-05-20 04:33:40 +0530

    added basic frontend

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express"); // Importing express module
 const bodyParser = require("body-parser");
 const async = require("async");
 const app = express(); // Creating an express object
-const axios = require('axios');
+const axios = require("axios");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 8000; // Setting an port for this application
+app.set("view engine", "ejs");
 
 async function weather(x) {
   let apiKey = "97e6009abb9c074341667c91ec83935e";
@@ -21,13 +22,28 @@ app.listen(port, function (err) {
   }
 });
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
+app.post("/getWeather", async function (req, res) {
+  let list = req.body.cities;
+  var city = list.split(",");
+  dict = {};
+  for (const element of city) {
+    const { data } = await weather(element);
+    dict[element] = data.main.temp;
+  }
+  var data = JSON.stringify(dict);
+  console.log(data);
+  res.render("out", { data: data });
+});
 
-app.post("/getWeather", async function(req, res) {
+app.post("/getWeatherapi", async function (req, res) {
   let city = req.body.cities;
-  dict ={};
-  for (const element of city){
-    const {data} = await weather(element);
+  dict = {};
+  for (const element of city) {
+    const { data } = await weather(element);
     dict[element] = data.main.temp;
   }
   res.send(dict);
